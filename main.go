662491984c6511ab9c0e55a082db8aaa425778b9
chunyukuo88 <alexgochenour@gmail.com>
2025-11-09 00:12:00 -0500

    feat: logger

--- main.go
+++ main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/chunyukuo88/rainy-day/logger"
+)
+
+func main() {
+	logInstance := initializeLogger()
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const address = ":8080"
+
+	err := http.ListenAndServe(address, nil)
+
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
+	}
+
+	logInstance.Info("Server is available")
+}
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("rainy-day.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize the logger: %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
